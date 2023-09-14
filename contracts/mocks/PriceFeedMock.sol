b0160156896e3f0da65f7cb1c756ca3430da23b6
not-thing <dade.dd@quarkonix.com>
2023-09-14 16:55:17 +0900

    WIP

---
+++ contracts/mocks/PriceFeedMock.sol
@@ -0,0 +1,147 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+import {AggregatorProxyInterface} from "@chromatic-protocol/contracts/oracle/interfaces/AggregatorProxyInterface.sol";
+
+contract PriceFeedMock is AggregatorProxyInterface {
+    uint256 private constant PHASE_OFFSET = 64;
+    struct RoundData {
+        uint80 roundId;
+        int256 answer;
+        uint256 startedAt;
+        uint256 updatedAt;
+        uint80 answeredInRound;
+    }
+
+    mapping(uint80 => RoundData) private roundDatas;
+    uint80 latestRoundId;
+    uint16 private currentPhaseId = 1;
+
+    constructor() {
+        latestRoundId = getStartingRoundId(currentPhaseId) - 1;
+    }
+
+    function increasePhase(int256 _answer) external {
+        currentPhaseId += 1;
+        latestRoundId = getStartingRoundId(currentPhaseId);
+        roundDatas[latestRoundId] = RoundData({
+            roundId: latestRoundId,
+            answer: _answer,
+            startedAt: block.timestamp,
+            updatedAt: block.timestamp,
+            answeredInRound: latestRoundId
+        });
+    }
+
+    function setRoundData(int256 _answer) external {
+        latestRoundId += 1;
+        roundDatas[latestRoundId] = RoundData({
+            roundId: latestRoundId,
+            answer: _answer,
+            startedAt: block.timestamp,
+            updatedAt: block.timestamp,
+            answeredInRound: latestRoundId
+        });
+    }
+
+    function latestAnswer() external view override returns (int256) {}
+
+    function latestTimestamp() external view override returns (uint256) {}
+
+    function latestRound() external view override returns (uint256) {}
+
+    function getAnswer(uint256 roundId) external view override returns (int256) {}
+
+    function getTimestamp(uint256 roundId) external view override returns (uint256) {}
+
+    function decimals() external pure override returns (uint8) {
+        return 18;
+    }
+
+    function description() external view override returns (string memory) {}
+
+    function version() external view override returns (uint256) {}
+
+    function getRoundData(
+        uint80 _roundId
+    )
+        external
+        view
+        override
+        returns (
+            uint80 roundId,
+            int256 answer,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        )
+    {
+        RoundData memory roundData = roundDatas[_roundId];
+        return (
+            roundData.roundId,
+            roundData.answer,
+            roundData.startedAt,
+            roundData.updatedAt,
+            roundData.answeredInRound
+        );
+    }
+
+    function latestRoundData()
+        external
+        view
+        override
+        returns (
+            uint80 roundId,
+            int256 answer,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        )
+    {
+        return this.getRoundData(latestRoundId);
+    }
+
+    function getStartingRoundId(uint16 _phaseId) internal pure returns (uint80) {
+        return uint80(uint256(_phaseId) << PHASE_OFFSET) + 1;
+    }
+
+    function phaseAggregators(uint16 /* phaseId */) external view override returns (address) {
+        return address(this);
+    }
+
+    function phaseId() external view override returns (uint16) {}
+
+    function proposedAggregator() external view override returns (address) {}
+
+    function proposedGetRoundData(
+        uint80 roundId
+    )
+        external
+        view
+        override
+        returns (
+            uint80 id,
+            int256 answer,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        )
+    {}
+
+    function proposedLatestRoundData()
+        external
+        view
+        override
+        returns (
+            uint80 id,
+            int256 answer,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        )
+    {}
+
+    function aggregator() external view override returns (address) {
+        return address(this);
+    }
+}
---
+++ test/forge/core/ChromaticLpTokenTest.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+import {Test} from "forge-std/Test.sol";
+import {IERC20Metadata} from "@openzeppelin/contracts/interfaces/IERC20Metadata.sol";
+import {IOracleProvider} from "@chromatic-protocol/contracts/oracle/interfaces/IOracleProvider.sol";
+import {CLBToken} from "@chromatic-protocol/contracts/core/CLBToken.sol";
+import {IChromaticMarket} from "@chromatic-protocol/contracts/core/interfaces/IChromaticMarket.sol";
+
+contract OracleProviderMock is IOracleProvider {
+    // add this to be excluded from coverage report
+    function test() public {}
+
+    function description() external pure override returns (string memory) {
+        return "ETH / USD";
+    }
+
+    function sync() external override returns (IOracleProvider.OracleVersion memory) {}
+
+    function currentVersion()
+        external
+        view
+        override
+        returns (IOracleProvider.OracleVersion memory)
+    {}
+
+    function atVersion(
+        uint256 oracleVersion
+    ) external view override returns (IOracleProvider.OracleVersion memory) {}
+}
+
+contract CLBTokenTest is Test, CLBToken {
+    OracleProviderMock public oracleProvider = new OracleProviderMock();
+
+    function setUp() public {
+        IERC20Metadata settlementToken = IERC20Metadata(address(1));
+        vm.mockCall(
+            address(settlementToken),
+            abi.encodeWithSelector(settlementToken.symbol.selector),
+            abi.encode("USDC")
+        );
+        vm.mockCall(
+            address(settlementToken),
+            abi.encodeWithSelector(settlementToken.decimals.selector),
+            abi.encode(6)
+        );
+
+        vm.mockCall(
+            address(market),
+            abi.encodeWithSelector(market.oracleProvider.selector),
+            abi.encode(address(oracleProvider))
+        );
+        vm.mockCall(
+            address(market),
+            abi.encodeWithSelector(market.settlementToken.selector),
+            abi.encode(address(settlementToken))
+        );
+    }
+
+    function testUri() public {
+        uint256 id = 1000;
+        // setImageUri("https://test.com/images/{id}.png");
+        // setImageUri(
+        //     "https:\\/\\/s3.amazonaws.com\\/your-bucket\\/images\\/{id}.png"
+        // );
+        emit log_string(uri(id));
+    }
+}
