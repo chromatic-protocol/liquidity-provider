bd28b4982204a6011a41606cd3cf2bb132da5be7
not-thing <dade.dd@quarkonix.com>
2023-09-15 18:44:44 +0900

    [WIP]: add `repl`

---
+++ hardhat/repl/type-extensions.ts
@@ -0,0 +1,11 @@
+// If your plugin extends types from another plugin, you should import the plugin here.
+
+// To extend one of Hardhat's types, you need to import the module where it has been defined, and redeclare it.
+import 'hardhat-deploy'
+import 'hardhat/types/runtime'
+
+declare module 'hardhat/types/runtime' {
+  interface HardhatRuntimeEnvironment {
+    deployLP?: () => Promise<void>
+  }
+}
