5dcf7578a8bfb1c34ee7d34ae1d6be484abd2d55
not-thing <dade.dd@quarkonix.com>
2023-10-06 15:57:30 +0900

    fix gasLimit and re-deploy Fixes #10

--- contracts/lp/base/mate2/ChromaticLPStorageMate2.sol
+++ contracts/lp/base/mate2/ChromaticLPStorageMate2.sol
@@ -36,7 +36,7 @@ abstract contract ChromaticLPStorageMate2 is ChromaticLPStorage, IMate2Automatio
     ) internal returns (uint256 upkeepId) {
         upkeepId = automate.registerUpkeep(
             address(this), // target
-            2e8, //uint32 gasLimit,
+            2e7, //uint32 gasLimit,
             address(this), // address admin,
             false, // bool useTreasury,
             singleExec, // bool singleExec,
