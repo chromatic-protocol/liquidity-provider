5140ef01b176da2ef1c9378e18e8688d3526ec1a
not-thing <dade.dd@quarkonix.com>
2023-10-24 14:06:56 +0900

    chore: remove deprecated

--- contracts/lp/libraries/LPStateValue.sol
+++ contracts/lp/libraries/LPStateValue.sol
@@ -4,7 +4,6 @@ pragma solidity >=0.8.0 <0.9.0;
 import {IERC1155} from "@openzeppelin/contracts/interfaces/IERC1155.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
-import {LiquidityBinValue} from "@chromatic-protocol/contracts/core/interfaces/market/Types.sol";
 import {LPState} from "~/lp/libraries/LPState.sol";
 import {ValueInfo} from "~/lp/interfaces/IChromaticLPLens.sol";
 import {BPS} from "~/lp/libraries/Constants.sol";
