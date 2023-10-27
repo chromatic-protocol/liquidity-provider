f80353f9f29d1a247149f8bcde4ab5fde0377ac6
not-thing <dade.dd@quarkonix.com>
2023-10-27 13:55:44 +0900

    feat: add `lpTag` in `IChromaticLPMeta`

--- contracts/lp/interfaces/IChromaticLPMeta.sol
+++ contracts/lp/interfaces/IChromaticLPMeta.sol
@@ -3,4 +3,6 @@ pragma solidity >=0.8.0 <0.9.0;
 
 interface IChromaticLPMeta {
     function lpName() external view returns (string memory);
+
+    function lpTag() external view returns (string memory);
 }
