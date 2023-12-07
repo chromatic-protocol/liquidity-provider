bacb9cd57a458033c703b8f9f61e35e416a36a2c
not-thing <dade.dd@quarkonix.com>
2023-12-07 14:34:48 +0900

    add LP distributionRates with proportion to `sqrt(index)` with supporting long/short only Fixes #120

--- hardhat/common/LPConfig.ts
+++ hardhat/common/LPConfig.ts
@@ -1,29 +1,17 @@
 import { parseEther } from 'ethers'
-import { getLinearDistributionConfig } from './DistributionRates'
+import { getSqrtDistributionConfig } from './DistributionRates'
 import type { LPConfig } from './types'
 
 // prettier-ignore
-const _FEE_RATES = [
+const FEE_RATES_LONG = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, // 0.01% ~ 0.09%, step 0.01%
     10, 20, 30, 40, 50, 60, 70, 80, 90, // 0.1% ~ 0.9%, step 0.1%
     100, 200, 300, 400, 500, 600, 700, 800, 900, // 1% ~ 9%, step 1%
     1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000 // 10% ~ 50%, step 5%
 ];
 
-const FEE_RATES = [...[..._FEE_RATES].reverse().map((x) => -x), ..._FEE_RATES]
-
-export function getDefaultDistribution(feeRates: number[]): number[] {
-  const sum = feeRates.reduce((s, x) => s + Math.abs(x), 0) // 63990
-
-  const BPS = 10000n
-  const MIN_RATE = 10n
-
-  let distributionRates = feeRates.map((x) => {
-    let r = (BigInt(Math.abs(x)) * BPS) / BigInt(sum)
-    return r < MIN_RATE ? Number(MIN_RATE) : Number(r)
-  })
-  return distributionRates
-}
+const FEE_RATES_SHORT = [...FEE_RATES_LONG].reverse().map((x) => -x)
+const FEE_RATES_ALL = [...FEE_RATES_SHORT, ...FEE_RATES_LONG]
 
 export function getDefaultLPConfigs(): LPConfig[] {
   const infos = [
@@ -33,7 +21,9 @@ export function getDefaultLPConfigs(): LPConfig[] {
       utilizationTargetBPS: 2500,
       startLevel: 0,
       endLevel: 50,
-      initialLiquidity: parseEther('30000')
+      feeRates: [FEE_RATES_ALL, FEE_RATES_LONG, FEE_RATES_SHORT],
+      isLongShorts: [0, 1, -1],
+      initialLiquidities: [parseEther('300000'), parseEther('150000'), parseEther('150000')]
     },
     {
       lpName: 'Plateau',
@@ -41,7 +31,9 @@ export function getDefaultLPConfigs(): LPConfig[] {
       utilizationTargetBPS: 5000,
       startLevel: 50,
       endLevel: 50,
-      initialLiquidity: parseEther('15000')
+      feeRates: [FEE_RATES_ALL, FEE_RATES_LONG, FEE_RATES_SHORT],
+      isLongShorts: [0, 1, -1],
+      initialLiquidities: [parseEther('150000'), parseEther('75000'), parseEther('75000')]
     },
     {
       lpName: 'Decresendo',
@@ -49,37 +41,42 @@ export function getDefaultLPConfigs(): LPConfig[] {
       utilizationTargetBPS: 7500,
       startLevel: 100,
       endLevel: 50,
-      initialLiquidity: parseEther('10000')
+      feeRates: [FEE_RATES_ALL, FEE_RATES_LONG, FEE_RATES_SHORT],
+      isLongShorts: [0, 1, -1],
+      initialLiquidities: [parseEther('100000'), parseEther('50000'), parseEther('50000')]
     }
   ]
   const lpConfigs = []
   for (let info of infos) {
-    const distInfo = getLinearDistributionConfig(info.startLevel, info.endLevel, _FEE_RATES.length)
-    // console.log('linear distribution information:', distInfo)
-    console.assert(
-      distInfo.utilizationTargetBPS == info.utilizationTargetBPS,
-      'check utilization target is correct'
-    )
-    const config = {
-      meta: {
-        lpName: info.lpName,
-        tag: info.tag
-      },
+    for (let i = 0; i < info.feeRates.length; i++) {
+      const feeRates = info.feeRates[i]
+      const distInfo = getSqrtDistributionConfig(
+        info.startLevel,
+        info.endLevel,
+        feeRates.length,
+        info.isLongShorts[i]
+      )
+      const config = {
+        meta: {
+          lpName: info.lpName,
+          tag: info.tag
+        },
 
-      config: {
-        utilizationTargetBPS: distInfo.utilizationTargetBPS,
-        rebalanceBPS: 500,
-        rebalanceCheckingInterval: 24 * 60 * 60, // 24 hours
-        settleCheckingInterval: 1 * 60, // 1 minutes
-        automationFeeReserved: parseEther('2.0'), // default
-        minHoldingValueToRebalance: parseEther('100.0')
-      },
-      feeRates: FEE_RATES,
-      distributionRates: distInfo.distributionRates,
-      initialLiquidity: info.initialLiquidity
+        config: {
+          utilizationTargetBPS: info.utilizationTargetBPS,
+          rebalanceBPS: 500,
+          rebalanceCheckingInterval: 24 * 60 * 60, // 24 hours
+          settleCheckingInterval: 1 * 60, // 1 minutes
+          automationFeeReserved: parseEther('2.0'), // default
+          minHoldingValueToRebalance: parseEther('100.0')
+        },
+        feeRates: feeRates,
+        distributionRates: distInfo.distributionRates,
+        initialLiquidity: info.initialLiquidities[i]
+      }
+
+      lpConfigs.push(config)
     }
-
-    lpConfigs.push(config)
   }
   return lpConfigs
 }
