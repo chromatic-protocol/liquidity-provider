50df212e5f359bddcf1b0eaab6f757f3c344764d
not-thing <dade.dd@quarkonix.com>
2024-01-09 16:21:24 +0900

    chore: rm unused tests

--- test/hardhat/oracle_provider/deploy.ts
+++
@@ -1,6 +0,0 @@
-import { OracleProviderMock } from '~/typechain-types'
-import { deployContract } from '../utils'
-
-export async function deploy(): Promise<OracleProviderMock> {
-  return await deployContract<OracleProviderMock>('OracleProviderMock')
-}
