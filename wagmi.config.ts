c2b56d641fdffcd1226a8c4983a2d207ec8749e8
not-thing <dade.dd@quarkonix.com>
2024-02-16 14:30:42 +0900

    fix: publish addresse of arbitrum_one Fixes #232

--- wagmi.config.ts
+++ wagmi.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from '@wagmi/cli'
 import { hardhat } from '@wagmi/cli/plugins'
+import { address as bpFactoryArbitrumOne } from '~/deployments/arbitrum_one/ChromaticBPFactory.json'
+import { address as registryArbitrumOne } from '~/deployments/arbitrum_one/ChromaticLPRegistry.json'
 import { address as bpFactoryArbitrumSepolia } from '~/deployments/arbitrum_sepolia/ChromaticBPFactory.json'
 import { address as registryArbitrumSepolia } from '~/deployments/arbitrum_sepolia/ChromaticLPRegistry.json'
 
@@ -17,10 +19,12 @@ export default defineConfig({
       ],
       deployments: {
         ChromaticLPRegistry: {
-          421614: registryArbitrumSepolia as `0x${string}`
+          421614: registryArbitrumSepolia as `0x${string}`,
+          42161: registryArbitrumOne as `0x${string}`
         },
         ChromaticBPFactory: {
-          421614: bpFactoryArbitrumSepolia as `0x${string}`
+          421614: bpFactoryArbitrumSepolia as `0x${string}`,
+          42161: bpFactoryArbitrumOne as `0x${string}`
         }
       }
     })
